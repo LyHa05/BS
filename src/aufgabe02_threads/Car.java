bc17bac167013b1f93ba89eb016a4888f1185447
lpflug <lpflug@NTCG57.ntcg.local>
2016-10-07 11:06:21 +0200

    Umsetzung der Car und SimRace mit Ergebnisausgabe

--- src/aufgabe02_threads/Car.java
+++ src/aufgabe02_threads/Car.java
@@ -1,22 +1,66 @@
 package aufgabe02_threads;
 
+import java.util.Random;
+
 public class Car extends Thread {
 	
 	private int startNummer;
 	private int gesamtFahrzeit;
+	private int anzahlZuFahrendeRunden;
+	private boolean rennenBeendet = false;
 	
-	public Car(int nummer) {
+	public Car(int nummer, int runden) {
 		this.startNummer = nummer;
+		this.anzahlZuFahrendeRunden = runden;
 	}
 
 	public void run() {
-		
-		
-		
+		try {
+			
+			int i = 1;
+			while (i <= anzahlZuFahrendeRunden) {
+				rundeFahren();
+				++i;
+			}
+			
+			rennenBeendet = true;
+			boolean inter = Thread.currentThread().interrupted();
+			
+			System.out.println("interrupt: " + inter);
+			
+		} catch (InterruptedException e) {
+			System.err.println("Eine InterruptedException!");
+			e.printStackTrace();
+		}
 	}
 	
-	public int getGesamtFahrzeit() {
+	private void rundeFahren() throws InterruptedException {
+		Random random = new Random();
+		int rundenZeit = random.nextInt(100 - 1) + 1;
+		// TODO Car oder Thread fuer Aufruf verwenden?
+		Thread.sleep(rundenZeit);
+		gesamtFahrzeit = gesamtFahrzeit + rundenZeit;
+	}
+
+	protected int getGesamtFahrzeit() {
 		return gesamtFahrzeit;
+	}
+	
+	private boolean getRennenBeendet() {
+		return rennenBeendet;
+	}
+	
+	protected int getStartNummer() {
+		return startNummer;
+	}
+
+	protected int compareTo(Car auto) {
+		
+		if(this.getGesamtFahrzeit() > auto.getGesamtFahrzeit()) {
+			return -1;
+		} else {
+			return 1;
+		}
 	}
 	
 }
--- src/aufgabe02_threads/SimRace.java
+++ src/aufgabe02_threads/SimRace.java
@@ -3,21 +3,30 @@ package aufgabe02_threads;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class SimRace {
 	
-	private int autoNummer = 0;
+	private static int anzahlRunden;
+	private int anzahlAutos;
+	private ArrayList<Car> autoTeilnehmer;
 	
-	public static void main(String[] args) throws IOException {
-		SimRace rennen = new SimRace();
-		rennen.beginnRennen();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		SimRace sr = new SimRace();
+		sr.rennenVorbereiten();
+		sr.rennenBeginnen();
+		
+//		while (!sr.rennenBeendet()) {
+//			// warten bis beendet
+//		}
+		
+		Thread.sleep(100 * anzahlRunden);
+		
+		sr.ergebnisAusgeben();
 	}
 	
-	private boolean eingabePruefen(String eingabe) {
-		return eingabe.matches("^[0-9]");
-	}
-	
-	private void beginnRennen() throws IOException {
+	private void rennenVorbereiten() throws IOException {
 		
 	    InputStreamReader isr = new InputStreamReader(System.in);
 	    BufferedReader br = new BufferedReader(isr);
@@ -35,10 +44,56 @@ public class SimRace {
 					+ "die im Rennen gefahren werden sollen.");
 			anzahlRunden = br.readLine();
 		} while (!eingabePruefen(anzahlRunden));
+	
+		this.anzahlAutos = Integer.parseInt(anzahlAutos);
+		this.anzahlRunden = Integer.parseInt(anzahlRunden);
+		
+	}
 
+	private void rennenBeginnen() {
+		
+		int i = 1;
+		autoTeilnehmer = new ArrayList<>();
+		
+		while (i <= anzahlAutos) {
+			Car auto = new Car(i,anzahlRunden);
+			autoTeilnehmer.add(auto);
+			auto.start();
+			++i;
+		}
+		
+	}
+
+	private boolean rennenBeendet() {
+		
+		boolean beendet = true;
+		
+		for(Car auto : autoTeilnehmer) {
+			if (!auto.isInterrupted()) {
+				beendet = false;
+			}
+		}
+		
+		return beendet;
+		
+	}
+
+	private void ergebnisAusgeben() {
+				
+		Collections.sort(autoTeilnehmer, (auto1,auto2) -> auto2.compareTo(auto1));
+		
+		System.out.println("\t **** Endstand ****");
+		
+		for (int j = 0; j < autoTeilnehmer.size(); j++) {
+
+			System.out.println((j+1) + ". Platz: \t Auto " + autoTeilnehmer.get(j).getStartNummer() + " \t Zeit: " + autoTeilnehmer.get(j).getGesamtFahrzeit());
+			
+		}
+		
+	}
+
+	private boolean eingabePruefen(String eingabe) {
+		return eingabe.matches("^[0-9]+");
 	}
 	
-	public int startNummerVergeben() {
-		return ++autoNummer;
-	}
 }
